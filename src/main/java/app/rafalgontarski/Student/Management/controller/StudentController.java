c22a14036ecfefb3ff1a71bbf766834064897ccd
RafalGontarski <gontarskirafal@gmail.com>
2023-07-05 15:13:27 +0200

    Student controller implementation.

--- src/main/java/app/rafalgontarski/Student/Management/controller/StudentController.java
+++ src/main/java/app/rafalgontarski/Student/Management/controller/StudentController.java
@@ -1,7 +1,21 @@
 package app.rafalgontarski.Student.Management.controller;
 
+import app.rafalgontarski.Student.Management.service.StudentService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StudentController {
+    private StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/students")
+    public String listStudents(Model model) {
+        model.addAttribute("students", studentService.getAllStudents());
+        return "students";
+    }
 }
