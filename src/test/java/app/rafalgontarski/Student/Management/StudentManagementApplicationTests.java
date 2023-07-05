42ba139e8045222f3f6562190d5b9814e5242a73
RafalGontarski <gontarskirafal@gmail.com>
2023-07-05 12:38:53 +0200

    Initial commit.

---
+++ src/test/java/app/rafalgontarski/Student/Management/StudentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package app.rafalgontarski.Student.Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
